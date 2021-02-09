8e9179eca7f0d421d14bfe14f9049b9d1dd78ee7
AshishBungla <bunglaahish28@gmail.com>
2021-02-09 19:31:20 +0530

    Test

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  function hideLoader() {
+    $('#loading').hide();
+  }
+  
+  $(window).ready(hideLoader);
+  
+  // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+  setTimeout(hideLoader, 20 * 1000);
+  
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
