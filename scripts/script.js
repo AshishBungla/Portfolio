7c8b1c9d4851544220125f746ce98bdb300569aa
ashish bungla <abungla@ar.iitr.ac.in>
2021-08-26 21:25:59 +0530

    26/8

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
     duration: 700
   });
 
+if (window.getComputedStyle(document.documentElement).scrollBehavior !== 'smooth') {
+  document.querySelectorAll('a[href^="#"]').forEach(internalLink => {
+      const targetElement = document.querySelector(internalLink.getAttribute('href'));
+      if (targetElement) {
+          internalLink.addEventListener('click', (e) => {
+              targetElement.scrollIntoView({
+                  behavior: 'smooth',
+              });
+              e.preventDefault();
+          });
+      }
+  });
+}
+
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
@@ -27,6 +41,31 @@ $(document).ready(function(){
       });
     } 
   }); 
+
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+      } else {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    },
+    );
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
+  }
+
+  );
+
+});
+
 
 
